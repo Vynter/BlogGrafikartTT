b29054f69ea3140bc3573e099f9dad09642673ff
Vynter <amine-cheraitia@hotmail.com>
2020-07-27 14:11:34 +0200

    a

--- views/post/show.php
+++ views/post/show.php
@@ -3,6 +3,9 @@
 use App\Connection;
 use App\Model\Category;
 use App\Model\Post;
+use App\Table\CategoryTable;
+use App\Table\PostTable;
+
 //astuce pour regroupé le use de category et post
 // use App\Model\{Category,Post};
 
@@ -10,19 +13,24 @@ $id = (int)($params)['id'];
 $slug = ($params)['slug'];
 
 $pdo = Connection::getPDO();
-$query = $pdo->prepare('SELECT * FROM post WHERE id = :id');
+
+$post = (new PostTable($pdo))->find($id);
+/* $query = $pdo->prepare('SELECT * FROM post WHERE id = :id');
 $query->execute(['id' => $id]);
 $query->setFetchMode(PDO::FETCH_CLASS, Post::class);
-$post = $query->fetch();
-/** vérification que l'utilisateur n'a pas touché a l'id */
+$post = $query->fetch(); 
+ vérification que l'utilisateur n'a pas touché a l'id 
 if ($post === false) {
     throw new Exception('aucun article ne corespend à ID que vous chercher');
-}
+}*/
 if ($post->getSlug() !== $slug) {
     $url = $router->url('post', ['id' => $post->getID(), 'slug' => $post->getSlug()]);
     http_response_code(301); // a revoir
     header('Location: ' . $url);
 }
+
+(new CategoryTable($pdo))->hydratePosts([$post]);
+/* 
 $query = $pdo->prepare(
     'SELECT c.id,c.slug,c.name 
     FROM post_category pc join category c on pc.category_id=c.id 
@@ -30,13 +38,13 @@ $query = $pdo->prepare(
 );
 $query->execute(['id' => $post->getID()]);
 $categories = $query->fetchAll(PDO::FETCH_CLASS, Category::class);
-
+ */
 ?>
 
 
 <h1 class="card-title"><?= e($post->getName()) ?></h1>
 <p class="text-muted"><?= $post->getCreatedAt()->format('d F Y') ?></p>
-<?php foreach ($categories as $k => $category) : ?>
+<?php foreach ($post->getCategories() as $k => $category) : ?>
 <?php if ($k > 0) : ?><?php echo ', '; ?><?php endif ?>
 <a
     href="<?= $router->url('category', ['id' => $category->getID(), 'slug' => $category->getSlug()]); ?>"><?= e($category->getName()) ?></a>
